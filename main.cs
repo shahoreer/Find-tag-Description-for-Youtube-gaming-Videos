e0509b3fb0c5bc1ac35777ee1003d5dbc0c432df
shahoreer <81192173+shahoreer@users.noreply.github.com>
2021-06-17 23:02:10 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -3,19 +3,26 @@ class HelloWorld {
   static void Main() {
       
       string g="King Archery";
+      string gl="shoot arrows towards a target ";
       
-    Console.WriteLine(g+" ->");
-    Console.WriteLine(ret(g));
+     Console.WriteLine(g+" TAG ->");
+     Console.WriteLine(ret(g));
+     Console.WriteLine(g+" DESCRIPTION ->");
+     Console.WriteLine(retd(g,gl));
   }
-  
   
   static string ret(string s){
       string rets;
-      
       rets=s+","+s+" Gameplay,"+ "Playing "+s+",Mobile Game,"+s+" Mobile Game,";
-      
       return rets;
-      
   }
-
+  
+  
+  static string retd(string g,string gl){
+      string ret;
+      ret = g+" is a free mobile game and Hyper Casual Game that you can play on your phone. In "+g+
+      " your goal is to " +gl+". I recommend playing "+g+
+      " if you want to play a free mobile game or Hyper Casual Game.";
+      return ret;
+  }
 }
