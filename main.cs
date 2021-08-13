b5a10454df8849aae242b08ddc8b69fbd537048f
shahoreer <81192173+shahoreer@users.noreply.github.com>
2021-08-13 10:47:17 -0400

    Update main.cs

--- main.cs
+++ main.cs
@@ -3,30 +3,37 @@ class HelloWorld {
   static void Main() {
 
       string game="King Archery";
-      string goal="Shoot arrows towards a target, and you get point for your accuracy";
-      string publisher="TGame";
-      string youtube_channel= "TalhaPro";
+      string goal="shoot arrows towards the target";
+      string publisher="tgame";
+      string youtube_channel= "TheBro";
+      string email= "thebroytm@gmail.com";
+      string gametype="2D Shooting";
       
      Console.WriteLine(game+" ->\n");
-     Console.WriteLine("TAG ->");
-     Console.WriteLine(ret(game));
-     Console.WriteLine("DESCRIPTION ->");
-     Console.WriteLine(retd(game,goal,publisher,youtube_channel));
+     Console.WriteLine("TAG ->\n");
+     Console.WriteLine(ret(game,gametype));
+     Console.WriteLine("\nDESCRIPTION ->\n");
+     Console.WriteLine(retd(game,goal,publisher,youtube_channel,email,gametype));
   }
   
-  static string ret(string s){
+  static string ret(string s,string t){
       string rets;
-      rets=s+","+s+" Gameplay,"+ "Playing "+s+",Mobile Game,"+s+" Mobile Game,";
+      rets=s+","+s+" Gameplay,"+ "Playing "+s+","+s+
+      " Mobile Game,"+t+" Games,Offline Games,Best Mobile Games,Best Offline Games,"+t+" offline Games,"+t+" Mobile Games,";
       return rets;
   }
   
-  static string retd(string g,string gl,string p,string c){
+  static string retd(string g,string gl,string p,string c,string e,string t){
       string ret;
-      ret = g+" is a free mobile game and Hyper Casual Game that you can play on your phone."+" The Game "+
+      ret = g+" is a free mobile game and "+t+ " Game that you can play on your phone." + " The Game "+
       g+" is made by "+p+". In "+ g+
       " your goal is to " +gl+". I recommend playing "+g+
-      " if you want to play a free mobile game or Hyper Casual Game.\n\n"+"Subscribe to "+c+" for more "+g+
-      " Gameplay.\n\n"+convinhashtag(g)+" "+convinhashtag(c)+" "+convinhashtag(c)+"videos "+convinhashtag(g)+"gameplay";
+      " if you want to play a free mobile game or "+t+" Game.\n\n"+
+      "Offline Games and Free Mobile Games are always fun, since "+g+" is a Offline Games and Free Mobile Games it's really a fun game to play. And I think "+
+      g+" is the best "+ t+" and best Free Mobile Games that you can play without internet connection. And "+
+      g+" is also the best offline mobile game and best "+t+"Game.\n\n"+
+      "Subscribe to "+c+" for more "+g+
+      " Gameplay and mobile "+t+" Games.\n\n"+convinhashtag(g)+" "+convinhashtag(c)+" "+convinhashtag(c)+"videos "+convinhashtag(g)+"gameplay\n\n"+"Email: "+e;
       return ret;
   }
   
